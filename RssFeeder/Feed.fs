0594a54a7e6f2745e9abe312ed442aed4c359153
Alan Green <algreengo@googlemail.com>
2012-11-21 22:12:03 +0000

    Creating Rss Feed Windows App

---
+++ RssFeeder/Feed.fs
@@ -0,0 +1,28 @@
+﻿module Feed
+
+open System.Net
+open System
+open System.IO
+
+let readFeed callback url =
+    let req = WebRequest.Create(Uri(url))
+    use resp = req.GetResponse()
+    use stream = resp.GetResponseStream()
+    use reader = new IO.StreamReader(stream)
+    callback reader url
+
+let myCallback (reader:IO.StreamReader) url = 
+    let html = reader.ReadToEnd()
+    html // return the html
+
+        // build a function with the callback "baked in"
+let fetchUrl1 = readFeed myCallback
+
+// all the sites i want to get the rss feed
+let sites   =   ["http://techcrunch.com/startups/";
+                "http://techcrunch.com/"]
+
+// display the content
+let displayContent  =   sites |>    List.map fetchUrl1
+
+
---
+++ RssFeeder/Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open Feed
+open System
+open System.Windows.Forms 
+open System.Drawing 
+ 
+//[<EntryPoint>]
+//let main argv = 
+ 
+let form=new Form()
+form.Text<- "My Window Application"
+form.BackColor<- Color.Gray  
+
+
+let richTextBox = new RichTextBox()
+//richTextBox.Text = Feed.displayContent
+let label = new Label()
+label.BackColor = Color.White
+label.Text = "Als Testing Windows form"
+label.Location = new Point(30,30)
+
+ 
+Application.Run(form)
+
+//    printfn "%A" argv
+//    Feed.sites   =   ["http://techcrunch.com/startups/";
+//                "http://techcrunch.com/"]
+ //   0 // return an integer exit code
