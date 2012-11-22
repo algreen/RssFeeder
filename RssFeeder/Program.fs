f52212b795074e4555a31e9a213528bcd67efc04
Alan Green <agreen@lizearle.com>
2012-11-22 14:08:34 +0000

    Win Form work

--- RssFeeder/Program.fs
+++ RssFeeder/Program.fs
@@ -4,27 +4,54 @@
 open Feed
 open System
 open System.Windows.Forms 
+open System.Windows.Forms.Design
 open System.Drawing 
  
-//[<EntryPoint>]
-//let main argv = 
- 
-let form=new Form()
-form.Text<- "My Window Application"
-form.BackColor<- Color.Gray  
+let label = 
+  let tempLabel = new Label()
+  do tempLabel.Name <- "LabelDescription"
+  do tempLabel.Text <- "Als Testing Windows Forms"
+  do tempLabel.Location <- new Drawing.Point(26,100)
+  tempLabel
+
+let richTextBox =
+  let myRichTextBox = new RichTextBox()
+  do myRichTextBox.Name <- "RichTextBox1"
+  do myRichTextBox.Text <- Feed.displayContent.ToString()
+  do myRichTextBox.ClientSize <- new System.Drawing.Size(300,300)
+  do myRichTextBox.DetectUrls <- true
+  myRichTextBox 
+
+//let webBrowser = 
+//  let myWebBrowser = new WebBrowser()
+//  do myWebBrowser.Text <- Feed.displayContent.ToString()
+//  myWebBrowser
+  
+let button = 
+  let feedButton = new Button()
+  do feedButton.Name <- ""
+  do feedButton.Text <- "Process Feed"
+  do feedButton.ClientSize <- new System.Drawing.Size(15,15)
+  feedButton
+
+let form =
+  let myForm = new Form()
+  do myForm.Name <- "Windows Application Test"
+  do myForm.BackColor <- Color.Gray
+  do myForm.Text <- "Time is...."
+  do myForm.ClientSize <- new System.Drawing.Size(293,266)
+  do myForm.Controls.Add(label) 
+  do myForm.Controls.Add(richTextBox)
+  do myForm.Controls.Add(button)
+//  do myForm.Controls.Add(webBrowser)
+  myForm
+
+let ticker = 
+  let temp = new Timer()
+  temp.Enabled <- true
+  temp.Interval <- 1000
+  temp.Tick.Add(fun _-> label.Text <- DateTime.Now.ToLongTimeString())
 
 
-let richTextBox = new RichTextBox()
-//richTextBox.Text = Feed.displayContent
-let label = new Label()
-label.BackColor = Color.White
-label.Text = "Als Testing Windows form"
-label.Location = new Point(30,30)
 
- 
-Application.Run(form)
-
-//    printfn "%A" argv
-//    Feed.sites   =   ["http://techcrunch.com/startups/";
-//                "http://techcrunch.com/"]
- //   0 // return an integer exit code
+do Application.Run(form)
